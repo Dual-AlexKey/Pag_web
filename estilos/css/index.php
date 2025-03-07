186d08e50a49e03902e8b0b35822674a524eb6c0
Dual-AlexKey <almissann@gmail.com>
2025-03-07 13:03:33 -0500

    Proceso Pagina

---
+++ estilos/css/index.php
@@ -0,0 +1,49 @@
+<link rel="stylesheet" type="text/css" href="styles.css?<?php echo time(); ?>" />
+
+<div class="Top-Container">
+    <!-- Grupo de Cuerpo-top-1 -->
+    <div class="Cuerpo-Group">
+        <div class="Cuerpo-top-1">Cuerpo top 1</div>
+        <div class="Cuerpo-top-1">Cuerpo top 1</div>
+    </div>
+
+    <!-- Grupo de Cuerpo-top-2 -->
+    <div class="Cuerpo-Group">
+        <div class="Cuerpo-top-2">Cuerpo top 2</div>
+        <div class="Cuerpo-top-2">Cuerpo top 2</div>
+    </div>
+
+    <!-- Grupo de Cuerpo-top-3 -->
+    <div class="Cuerpo-Group">
+        <div class="Cuerpo-top-3">Cuerpo top 3</div>
+    </div>
+</div>
+ 
+<div class="Columna-Container">
+    <div class="Columna_Izquierda">Columna Izquierda</div>
+    <div class="Columna_Central">Columna Central</div>
+    <div class="Columna_Derecha">Columna Derecha</div>
+</div>
+
+<div class="Top-Container">
+    <!-- Grupo de Cuerpo-top-1 -->
+    <div class="Cuerpo-Group">
+        <div class="Cuerpo-top-1">Cuerpo buttom  1</div>
+        <div class="Cuerpo-top-1">Cuerpo buttom 1</div>
+    </div>
+
+    <!-- Grupo de Cuerpo-top-2 -->
+    <div class="Cuerpo-Group">
+        <div class="Cuerpo-top-2">Cuerpo buttom 2</div>
+        <div class="Cuerpo-top-2">Cuerpo buttom 2</div>
+    </div>
+
+    <!-- Grupo de Cuerpo-top-3 -->
+    <div class="Cuerpo-Group">
+        <div class="Cuerpo-top-3">Cuerpo buttom 3</div>
+    </div>
+</div>
+
+
+
+
